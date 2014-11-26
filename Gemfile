68598b9ed2900c3708553be64981caa6c67086d0
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2014-11-26 12:15:16 -0600

    Add brewery_db library/api key to project.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org/'
 gem 'sinatra'
 gem 'sinatra-contrib', require: 'sinatra/reloader'
 gem 'pry-byebug'
+gem 'brewery_db'
+gem 'dotenv'
 
 group :test do
   gem 'rack-test'
--- web/server.rb
+++ web/server.rb
@@ -1,9 +1,23 @@
+require_relative '../lib/breweries.rb'
 require 'sinatra'
+require 'brewery_db'
+require 'pry-byebug'
+require 'dotenv'
 
 class Breweries::Server < Sinatra::Application
 
+  Dotenv.load 
+
+  brewery_db = BreweryDB::Client.new do |config|
+    config.api_key = ENV['BREWERY_DB_KEY']
+  end
+
   get '/' do
+    @breweries = brewery_db.locations.all(locality: 'San Francisco')
+    binding.pry
     erb :index
   end
-  
+
+  run! if __FILE__ == $0
+
 end
